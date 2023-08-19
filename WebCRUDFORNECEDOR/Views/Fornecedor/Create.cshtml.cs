2ab9b26635861ec00d43bb82c5344d203c17943b
Felipe Sacramento <mzssacramento@gmail.com>
2023-08-19 02:17:41 -0300

    fix index! found!

---
+++ WebCRUDFORNECEDOR/Views/Fornecedor/Create.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebCRUDFORNECEDOR.Views.Fornecedor
+{
+    public class CreateModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
---
+++ WebCRUDFORNECEDOR/Views/Fornecedor/Details.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebCRUDFORNECEDOR.Views.Fornecedor
+{
+    public class DetailsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
